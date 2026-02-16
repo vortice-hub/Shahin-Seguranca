b4de14cd884a8bff56b966f1200a8cf9d3a31e4a
AppMotoristaPro <appmotoristapro@gmail.com>
2026-02-16 09:55:11 -0300

    Finalização do horario flexivel

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -41,9 +41,13 @@ def primeiro_acesso():
 @auth_bp.route('/cadastrar', methods=['GET', 'POST'])
 def auto_cadastro():
     if request.method == 'GET': return render_template('auth/auto_cadastro.html', step=1)
+    
     if request.method == 'POST':
-        cpf = request.form.get('cpf').replace('.', '').replace('-', '').strip()
+        cpf_input = request.form.get('cpf', '')
+        cpf = cpf_input.replace('.', '').replace('-', '').strip()
+        
         pre = PreCadastro.query.filter_by(cpf=cpf).first()
+        
         if not pre:
             if User.query.filter_by(cpf=cpf).first():
                 flash('Você já tem cadastro. Faça login.')
@@ -54,9 +58,39 @@ def auto_cadastro():
         password = request.form.get('password')
         if password:
             username = gerar_login_automatico(pre.nome_previsto)
-            while User.query.filter_by(username=username).first(): username = gerar_login_automatico(pre.nome_previsto)
-            novo_user = User(username=username, password_hash=generate_password_hash(password), real_name=pre.nome_previsto, role=pre.cargo, cpf=cpf, salario=pre.salario, horario_entrada=pre.horario_entrada, horario_almoco_inicio=pre.horario_almoco_inicio, horario_almoco_fim=pre.horario_almoco_fim, horario_saida=pre.horario_saida, escala=pre.escala, data_inicio_escala=pre.data_inicio_escala, is_first_access=False)
-            db.session.add(novo_user); db.session.delete(pre); db.session.commit()
+            # Garante username único
+            while User.query.filter_by(username=username).first(): 
+                username = gerar_login_automatico(pre.nome_previsto) + str(db.session.query(User).count())
+            
+            novo_user = User(
+                username=username, 
+                password_hash=generate_password_hash(password), 
+                real_name=pre.nome_previsto, 
+                role=pre.cargo, 
+                cpf=cpf, 
+                salario=pre.salario, 
+                
+                # --- NOVOS DADOS COPIADOS (JORNADA E EMPRESA) ---
+                razao_social_empregadora=pre.razao_social,
+                cnpj_empregador=pre.cnpj,
+                carga_horaria=pre.carga_horaria,
+                tempo_intervalo=pre.tempo_intervalo,
+                inicio_jornada_ideal=pre.inicio_jornada_ideal,
+                # ------------------------------------------------
+                
+                horario_entrada=pre.horario_entrada, # Mantido por legado
+                horario_saida=pre.horario_saida,
+                escala=pre.escala, 
+                data_inicio_escala=pre.data_inicio_escala, 
+                is_first_access=False
+            )
+            
+            db.session.add(novo_user)
+            db.session.delete(pre) # Remove da fila
+            db.session.commit()
+            
             return render_template('auth/auto_cadastro_sucesso.html', username=username, nome=pre.nome_previsto)
         else:
             return render_template('auth/auto_cadastro.html', step=2, cpf=cpf, nome=pre.nome_previsto)
+
+
